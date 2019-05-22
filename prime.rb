1d0e5e3eaa3725f793de9cc8b728b60fc63a0c8e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 03:38:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,17 @@
 # Add  code here!
 def prime?(n)
-  if (n < 2)
+  if (n < 2) || (n % 2 == 0)
+    return false
+  end
+  
+  a = rand(1..(n - 1))
+
+  left = a**((n-1)/2)
+  right = (a/n) % n
+
+  if (left != right)
     return false
   end
 
-  a = rand(1..(n - 1))
-  puts a
-
+  return true
 end
