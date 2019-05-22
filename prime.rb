866c2f68feaff0db50e707525b9408bdbacdcdc3
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 03:15:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
 # Add  code here!
 def prime?(number)
-  (number < 0) || (number % 2 == 0 && number != 2) || (number % 3 == 0 && number != 3) ? false : true
+  (number < 2) || (number % 2 == 0 && number != 2) || (number % 3 == 0 && number != 3) ? false : true
 end
